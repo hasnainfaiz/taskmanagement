445cadabd859acd472d32504b237187662e33bc3
Phaedra Solutions <phaedrasolutions@Phaedras-MacBook-Pro.local>
2023-06-06 18:30:00 +0500

    new changes pushed

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,24 +12,6 @@ export class TasksService {
   getTasks(filterDto: GetTasksFilterDto) {
     return this.taskRepository.getTasks(filterDto);
   }
-  // private tasks: Task[] = [];
-  // // getAllTasks(): Task[] {
-  // //   return this.tasks;
-  // // }
-  // // getTaskWithFilters(filterDto: GetTasksFilterDto) {
-  // //   const { status, search } = filterDto;
-  // //   let tasks = this.getAllTasks();
-  // //   if (status) {
-  // //     tasks = tasks.filter((task) => task.status === status);
-  // //   }
-  // //   if (search) {
-  // //     tasks = tasks.filter(
-  // //       (task) =>
-  // //         task.title.includes(search) || task.description.includes(search),
-  // //     );
-  // //   }
-  // //   return tasks;
-  // // }
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne({ where: { id: id } });
     if (!found) {
