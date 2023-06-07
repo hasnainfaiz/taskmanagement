989234c95afb4b606252b279fd747bdbed6bd707
Phaedra Solutions <phaedrasolutions@Phaedras-MacBook-Pro.local>
2023-06-07 16:34:42 +0500

    new changes pushed

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -47,8 +47,8 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(@Param('id', ParseIntPipe) id: number, @Req() req): Promise<void> {
+    return this.tasksService.deleteTask(id, req.user);
   }
 
   @Patch('/:id/status')
